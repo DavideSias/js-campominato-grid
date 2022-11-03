73568ada45d111a23f7137f8a2e6f7cb02ee6dde
DavideSias <davidesias77@gmail.com>
2022-11-03 10:54:32 +0100

    Refactor js

--- js/script.js
+++ js/script.js
@@ -16,29 +16,34 @@ const elePlay = document.querySelector('.btn-play');
 elePlay.addEventListener('click', function(){
 
     const eleGrid = document.querySelector('.grid');
-    eleGrid.replaceChildren();
+    eleGrid.replaceChildren();  
+    // eleGrid.innerHTML = '' alternativa per svuotare il contenuto
     const eleDifficulty = document.querySelector('#difficulty').value;
-    
+
     
     if (eleDifficulty === 'easy'){
-        for (let i = 1; i < 101; i++) {
+        for (let i = 1; i <= 100; i++) {
             eleCell = document.createElement('div');
-            eleCell.classList.toggle('cell-100');
+            eleCell.classList.add('cell');
             eleGrid.append(eleCell);
-            eleCell.innerHTML = [i];   
-
+            eleCell.innerHTML = i; 
+            eleCell.style.width = `calc(100% / 10)`;
+            eleCell.style.style = `calc(100% / 10)`;  
+            
             eleCell.addEventListener('click', function() {
                 this.classList.toggle('active');
                 console.log([i]);
             });                
         }    
     } else if (eleDifficulty === 'medium'){
-        for (let i = 1; i < 82; i++) {
+        for (let i = 1; i <= 81; i++) {
         
             eleCell = document.createElement('div');
-            eleCell.classList.add('cell-81');
+            eleCell.classList.add('cell');
             eleGrid.append(eleCell);
-            eleCell.innerHTML = [i];
+            eleCell.innerHTML = i;
+            eleCell.style.width = `calc(100% / 9)`;
+            eleCell.style.style = `calc(100% / 9)`; 
         
             eleCell.addEventListener('click', function() {
                 this.classList.toggle('active');
@@ -46,12 +51,14 @@ elePlay.addEventListener('click', function(){
             });                
         }
     } else if (eleDifficulty === 'hard'){
-        for (let i = 1; i < 50; i++) {
+        for (let i = 1; i <= 49; i++) {
           
             eleCell = document.createElement('div');
-            eleCell.classList.add('cell-49');
+            eleCell.classList.add('cell');
             eleGrid.append(eleCell);
-            eleCell.innerHTML = [i];
+            eleCell.innerHTML = i;
+            eleCell.style.width = `calc(100% / 7)`;
+            eleCell.style.style = `calc(100% / 7)`; 
         
             eleCell.addEventListener('click', function() {
                 this.classList.toggle('active');
